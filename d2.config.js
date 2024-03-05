217508ddb79ffbf17ff0449704a565bbb3a3ef96
chisom chima <chimachisom360@gmail.com>
2024-03-05 12:34:34 +0100

    feat: prepare for release

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,6 @@
 const config = {
     id: '22229dce-6f0e-48e0-aa41-4c1bbe683ae2',
-    minDHIS2Version: '99.9.9',
+    minDHIS2Version: '2.41',
     type: 'app',
     name: 'menu-management',
     title: 'Menu Management',
